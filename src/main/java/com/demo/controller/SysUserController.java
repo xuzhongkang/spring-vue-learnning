b5661cedbb1ce6c0f3cd1f30fb6881feb365091a
JH025406 <zhongkang.xu@bestlink.com.cn>
2019-12-16 16:41:18 +0800

    refactor:增加swagger注释

--- src/main/java/com/demo/controller/SysUserController.java
+++ src/main/java/com/demo/controller/SysUserController.java
@@ -4,83 +4,61 @@ import com.demo.entity.RestResult;
 import com.demo.entity.SysUser;
 import com.demo.service.SysUserService;
 import com.demo.utils.ResultGenerator;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
 /**
- * 用户控制层
- * . @RestController 该类下所有返回值默认以json格式进行返回
- * . @Validated 代表该类启用参数验证，通过添加注解可以验证参数
+ * 用户controller层
+ * .@Validated 代表该类启用参数验证，通过添加注解可以验证参数
+ *
  * @author xuzhongknag on 2019/12/07.
  */
+
+@Api(tags = "用户信息controller")
 @RestController
 @RequestMapping("/user")
 @Validated
 public class SysUserController {
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
-    private final SysUserService userService;
-
-    private final ResultGenerator generator;
-
     @Autowired
-    public SysUserController(SysUserService userService, ResultGenerator generator) {
-        this.userService = userService;
-        this.generator = generator;
-    }
+    private SysUserService userService;
+    @Autowired
+    private ResultGenerator generator;
 
-    /**
-     * .在实体前添加 @Valid 注解代表要对这个实体进行验证，如果验证不通过就会报异常
-     * bindingResult是对异常信息的包装，不过这里我们不用，而是交由异常处理器进行处理
-     * @return user
-     */
+    @ApiOperation(httpMethod = "GET", value = "注册用户接口")
+    @ApiImplicitParam(name = "SysUser", value = "用户对象", required = true, dataType = "Object")
     @RequestMapping("/register")
     public RestResult register(@Valid SysUser user, BindingResult bindingResult) {
-        return generator.getSuccessResult("用户注册成功",userService.saveUser(user));
+        return generator.getSuccessResult("用户注册成功", userService.saveUser(user));
     }
 
-    /**
-     * 。@NotNull 在字段前添加注解代表验证该字段，如果为空则报异常
-     * @return 登陆成功则返回相关信息，否则返回错误提示
-     */
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public RestResult login(String name,String password) {
-        System.out.println("name:"+name+";password:"+password);
+
+    @ApiOperation(httpMethod = "GET", value = "用户登录接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name", value = "用户名", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "password", value = "密码", required = true, dataType = "String")
+    })
+    @PostMapping(value = "/login")
+    public RestResult login(String name, String password) {
+        System.out.println("name:" + name + ";password:" + password);
         SysUser user = userService.checkLogin(name, password);
-        if(user != null) {
-            return generator.getSuccessResult("登陆成功",user);
+        if (user != null) {
+            return generator.getSuccessResult("登陆成功", user);
         }
         return generator.getFailResult("用户名/密码错误");
     }
 
-    /**
-     * 为参数验证添加异常处理器
-     */
-    @ExceptionHandler(ConstraintViolationException.class)
-    public RestResult handleConstraintViolationException(ConstraintViolationException cve) {
-        String errorMessage = cve.getConstraintViolations().iterator().next().getMessage();
-        return generator.getFailResult(errorMessage);
-    }
-
-    /**
-     * 用户名重复
-     */
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public RestResult handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
-        //如果注册两个相同的用户名到报这个异常
-        return generator.getFailResult("用户名重复");
-    }
 }
